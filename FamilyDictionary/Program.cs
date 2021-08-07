fc3d61055c2a0e309bdd3379f4335ac6eee95557
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-08-07 09:46:02 -0500

    Add project files.

---
+++ FamilyDictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FamilyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+            myFamily.Add("Brother", new Dictionary<string, string>() { { "name", "Emmanuel" }, { "age", "27" } });
+            myFamily.Add("Mother", new Dictionary<string, string>() { { "name", "Annie" }, { "age", "60" } });
+            myFamily.Add("Father", new Dictionary<string, string>() { { "name", "Charles" }, { "age", "60" } });
+
+            foreach(var member in myFamily)
+            {
+                var name = member.Value["name"];
+                var age = member.Value["age"];
+                Console.WriteLine($"{name} is my {member.Key} and is {age} years old.");
+            }
+
+        }
+    }
+}
